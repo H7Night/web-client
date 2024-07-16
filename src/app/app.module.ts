d43d15b78b6f4399d75432a6739a6e643a8ccdb2
hezhongxian <jhonhe7night@outlook.com>
2024-07-16 23:36:33 +0800

    更新material 18.1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 
-import {ErrorCodeComponent} from './shared/components/error-code/error-code.component';
 import {MaterialModule} from './material.module';
 import {RoutesRoutingModule} from './routes/routes-routing.module';
 import {LoginComponent} from './login/login/login.component';
@@ -14,7 +13,6 @@ import {DashboardComponent} from './view/dashboard/dashboard.component';
 @NgModule({ declarations: [
         AppComponent,
         LoginComponent,
-        ErrorCodeComponent,
         DashboardComponent
     ],
     bootstrap: [AppComponent], imports: [BrowserModule,
