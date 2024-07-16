fdf8cf4dd4346fe1b400c0ffc48e67fffff323ef
hezhongxian <jhonhe7night@outlook.com>
2024-07-16 23:14:46 +0800

    更新angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {HttpClientModule, HttpClient} from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
@@ -11,23 +11,16 @@ import {RoutesRoutingModule} from './routes/routes-routing.module';
 import {LoginComponent} from './login/login/login.component';
 import {DashboardComponent} from './view/dashboard/dashboard.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    ErrorCodeComponent,
-    DashboardComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    FormsModule,
-    RoutesRoutingModule,
-    MaterialModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LoginComponent,
+        ErrorCodeComponent,
+        DashboardComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        RoutesRoutingModule,
+        MaterialModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {
 }
